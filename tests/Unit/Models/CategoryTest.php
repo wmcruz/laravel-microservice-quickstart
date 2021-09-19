2c8e584bcb089b25684af2bf04259caaef85b273
Wellington Max Cruz <wellington.max1@gmail.com>
2021-09-18 22:41:13 -0300

    Criaçao de testes para Category.

---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Genre;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    private $category;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->category = new Category();
+    }
+
+    public function testIfUseTraitsAttribute() {
+        $traits = [
+            SoftDeletes::class,
+            Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+    public function testFillableAttribute() {
+        $fillable = ['name', 'description', 'is_active'];
+        $this->assertEquals($fillable, $this->category->getFillable());
+    }
+
+
+    public function testCastsAttribute() {
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $this->assertEquals($casts, $this->category->getCasts());
+    }
+
+    public function testIncrementingAttribute() {
+        $this->assertFalse($this->category->incrementing);
+    }
+
+    public function testDatesAttribute() {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        foreach ($dates as $date) {
+            $this->assertContains($date, $this->category->getDates());
+        }
+
+        $this->assertCount(count($dates), $this->category->getDates());
+    }
+}
