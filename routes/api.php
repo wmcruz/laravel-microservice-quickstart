a92bc45019b48c72720c0967c93c536250a9c3a7
Wellington Max Cruz <wellington.max1@gmail.com>
2021-10-10 18:24:27 -0300

    Adicionando nova rota e constante para as rotas.

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
+    $exceptCreateAndExist = ['except' => ['create', 'edit']];
+
+    Route::resource('categories', 'CategoryController', $exceptCreateAndExist);
+    Route::resource('genres', 'GenreController', $exceptCreateAndExist);
+    Route::resource('cast_members', 'CastMemberController', $exceptCreateAndExist);
 });
