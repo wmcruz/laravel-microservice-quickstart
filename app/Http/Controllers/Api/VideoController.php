6d84438c19d985c23c443305b31bac60446ed176
Wellington Max Cruz <wellington.max1@gmail.com>
2021-11-15 15:54:56 -0300

    Adicionando relacionamento entre Video e Categorias e Generos.

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BasicCrudController;
 use App\Models\Video;
+use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController {
 
@@ -17,7 +18,29 @@ class VideoController extends BasicCrudController {
             'opened' => 'boolean',
             'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id',
+            'genres_id' => 'required|array|exists:genres,id'
         ];
+    }
+
+    public function store(Request $request) {
+        $validateData = $this->validate($request, $this->rulesStore());
+        $obj = $this->model()::create($validateData);
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        $obj->refresh();
+
+        return $obj;
+    }
+
+    public function update(Request $request, $id) {
+        $obj = $this->findOrFail($id);
+        $validateDate = $this->validate($request, $this->rulesUpdate());
+        $obj->update($validateDate);
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+
+        return $obj;
     }
 
     protected function model() {
